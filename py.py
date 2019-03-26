836ae42aa19a19bb1c29ef8d662e7eeea41ecf1e
daming98 <304672017@qq.com>
2019-03-26 23:36:54 +0800

    Add the Exception capture

--- py.py
+++ py.py
@@ -3,55 +3,56 @@ from selenium import webdriver
 import time
 import random
 
-username="bbming1@163.com"
-uid="5848719965"
-password=""
+def Login():
+    username="1429077642@qq.com"
+    uid="5706670755"
+    password="duoxihuanni123"
 
-chrome_driver="D:\Python\ChromeDrive\chromedriver.exe"
+    chrome_driver="D:\Python\ChromeDrive\chromedriver.exe"
 
-browser = webdriver.Chrome(executable_path=chrome_driver)
+    browser = webdriver.Chrome(executable_path=chrome_driver)
 
-browser.get("http://www.weibo.com/")
-browser.maximize_window()
-time.sleep(7)
-# input id="loginname" name="username"
-# input id="password"
+    browser.get("http://www.weibo.com/")
+    browser.maximize_window()
+    time.sleep(7)
+    # input id="loginname" name="username"
+    # input id="password"
 
-input_username=browser.find_element_by_id("loginname")
-input_username.clear()
-input_username.send_keys(username)
+    input_username=browser.find_element_by_id("loginname")
+    input_username.clear()
+    input_username.send_keys(username)
 
-input_password=browser.find_element_by_name("password")
-input_password.clear()
-input_password.send_keys(password)
+    input_password=browser.find_element_by_name("password")
+    input_password.clear()
+    input_password.send_keys(password)
 
-time.sleep(2)
-	
-codeimg=browser.find_element_by_xpath("//img[@node-type='verifycode_image']")
-if(codeimg.get_attribute("src")=="about:blank"):
-	browser.find_element_by_class_name("W_btn_a").click()
-else:
-	input_code=browser.find_element_by_xpath("//input[@value='验证码']")
-	input_code.clear()
-	code=input("Input Code：")
-	input_code.send_keys(code)
-	browser.find_element_by_class_name("W_btn_a").click()
-	
-time.sleep(1)
+    time.sleep(2)
+        
+    codeimg=browser.find_element_by_xpath("//img[@node-type='verifycode_image']")
+    if(codeimg.get_attribute("src")=="about:blank"):
+        browser.find_element_by_class_name("W_btn_a").click()
+    else:
+        input_code=browser.find_element_by_xpath("//input[@value='验证码']")
+        input_code.clear()
+        code=input("Input Code：")
+        input_code.send_keys(code)
+        time.sleep(1)
+        browser.find_element_by_class_name("W_btn_a").click()
+    time.sleep(1)
 
-while("输入的验证码不正确" in browser.page_source):
-	print("Wrong Verification Code")
-	input_code.clear()
-	code=input("Input Code：")
-	input_code.send_keys(code)
-	browser.find_element_by_class_name("W_btn_a").click()
-	
-time.sleep(1)
+    while("输入的验证码不正确" in browser.page_source or "请填写" in browser.page_source):
+        print("Wrong Verification Code")
+        input_code.clear()
+        code=input("Input Code：")
+        input_code.send_keys(code)
+        browser.find_element_by_class_name("W_btn_a").click()
+        
+    time.sleep(1)
 
-if "我的收藏" in browser.page_source:
-	print("Login Successfully")
-else:
-	print("Login failed")
+    browser.get("https://weibo.com/u/5706670755/home")
+    
+    time.sleep(1)
+    return browser
 
 
 def post(n, txt):
@@ -73,7 +74,7 @@ def post(n, txt):
 
 #post(20, "test")
 
-def delete():
+def deleteALL():
 	time.sleep(3)
 	browser.find_element_by_xpath("//a[@nm='name']").click()
 	time.sleep(3)
@@ -93,33 +94,85 @@ def delete():
 		browser.refresh()
 		time.sleep(2)
 
-def delete(n):
-	time.sleep(3)
-	browser.find_element_by_xpath("//a[@nm='name']").click()
-	time.sleep(3)
-	browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-	browser.execute_script('window.scrollTo(0, document.head.scrollHeight)')
-	del_list1=browser.find_elements_by_xpath("//a[@action-type='fl_menu']")
-	del_list2=browser.find_elements_by_xpath("//a[@action-type='feed_list_delete']")
-	if(n>len(del_list1)):
-		print("一次最多删除一页的微博，你输入的数太大了。如果你想全部删除，delete()不加参数。")
-		return
-	#time.sleep(1)
-	for i in range(n):
-		del_list1[i].click()
-		time.sleep(1)
-		del_list2[i].click()
-		#time.sleep(1)
-		browser.find_element_by_xpath("//a[@action-type='ok']").click()
-		time.sleep(1)
-		print("Delete %d Weibo Successfully, %d Weibo waitting to delete." % (i+1, n-i-1))
-	print("Successfully")
-	#browser.refresh()
-	#time.sleep(2)
+
+def delete(n, browser):
+    time.sleep(3)
+    browser.find_element_by_xpath("//a[@nm='name']").click()
+    time.sleep(5)
+    count=0
+    for i in range(n):
+        #time.sleep(1)
+        while True:
+            try:
+                click1=browser.find_element_by_xpath("//a[@action-type='fl_menu']")
+            except NoSuchElementException:
+                print("Can't find the menu. wait 2 s.")
+                sleep(2)
+            except Exception:
+                print("Exception, refresh and retry. ")
+                return "Exception"
+            else:
+                try:
+                    click1.click()
+                except Exception:
+                    print("Exception, refresh and retry. ")
+                    return "Exception"
+                else:
+                    break
+        time.sleep(0.5)
+        while True:
+            try:
+                click2=browser.find_element_by_xpath("//a[@action-type='feed_list_delete']")
+            except NoSuchElementException:
+                print("Can't find the del btn. wait 2 s.")
+                sleep(2)
+            except Exception:
+                print("Exception, refresh and retry. ")
+                return "Exception"
+            else:
+                try:
+                    click2.click()
+                except Exception:
+                    print("Exception, refresh and retry. ")
+                    return "Exception"
+                else:
+                    break
+        time.sleep(1)
+        while True:
+            try:
+                click3=browser.find_element_by_xpath("//a[@action-type='ok']")
+            except NoSuchElementException:
+                print("Can't find the ok btn. wait 2 s.")
+                sleep(2)
+            except Exception:
+                print("Exception, refresh and retry. ")
+                return "Exception"
+            else:
+                try:
+                    click3.click()
+                except Exception:
+                    print("Exception, refresh and retry. ")
+                    return "Exception"
+                else:
+                    break
+        time.sleep(1)
+        count+=1
+        while(count==10):
+            browser.refresh()
+            time.sleep(5)
+            count=0
+        print("Delete %d Weibo Successfully, %d Weibo waitting to delete." % (i+1, n-i-1))
+    print("Successfully")
+    return 1
 
 # 一次最多删除一页的微博，如果想全部删除，不加参数。
-delete(1)
-	
-#browser.get("http://www.weibo.com/5848719965")
-
-#print(browser.page_source)
+def main():
+    browser=Login()
+    if browser is not None:
+        while True:
+            flag=delete(1100, browser)
+            if(flag=="Exception"):
+                browser.refresh()
+            else:
+                break
+main()
