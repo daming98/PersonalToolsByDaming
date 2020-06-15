31a89822fc15690603055e87b35fca986b8e92b7
daming98 <304672017@qq.com>
2020-06-15 22:23:13 +0800

    Add files via upload

---
+++ SinaBlogCrawler/vae_blog.py
@@ -0,0 +1,189 @@
+l=['http://blog.sina.com.cn/s/blog_4a90c0b80102dyn8.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80102dxnv.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801017q56.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801017pzu.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801017pk8.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100mdsg.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100le34.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100kopx.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100knte.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100kauq.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100jace.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100j7id.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100j4cf.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100iuq5.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100ib53.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100gsyk.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100gmqg.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100glpa.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100gjc2.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100gg28.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100g95s.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100fyos.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100fkih.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100f9qa.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100exhw.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100esjm.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100es4m.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100egt4.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100e7m8.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100du7d.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100dlav.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100dbm4.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100d4yd.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100cvk8.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100cqvw.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100cl1b.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100chfl.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100cbue.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100c27t.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100buhg.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100bmi6.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100bbmb.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100b4nq.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100awyd.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100akkb.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100aihf.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100a9ax.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801009xvy.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801009wzl.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801009v3m.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801009uaa.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801009s74.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801009l1u.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801009ic0.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801009ejh.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010097jf.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010094gm.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010092mz.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801008vej.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801008th3.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801008kpr.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801008h5r.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801008de8.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010080po.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801007ymq.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801007x1k.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801007vv9.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000by7.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000bxz.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000btl.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000brh.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000bm0.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000bhp.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000bh2.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000bgp.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000ben.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000bel.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000be3.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000b9u.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000b9e.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000b5i.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000b03.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000avk.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000aip.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000aen.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000acc.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000a9r.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000a3t.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010009zz.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010009pv.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010009nt.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010009hs.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100094g.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100091z.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010008x9.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010008x2.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010008nf.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010008j8.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010008hd.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100086t.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000846.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010007uv.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010007pg.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010007ov.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010007m9.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010007l0.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010007c6.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100076m.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100076g.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010006oo.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010006mk.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010006lj.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010006jm.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010006j1.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010006i6.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010006g0.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010006et.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010006dj.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010006cx.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010006ag.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b801000658.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b80100064c.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010005zu.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010005zv.html', \
+'http://blog.sina.com.cn/s/blog_4a90c0b8010005zx.html']
+
+import urllib.request
+import chardet
+import re
+
+def get_artical(url):
+    
+    response = urllib.request.urlopen(url)
+
+    html = response.read()
+    chardit1 = chardet.detect(html)
+    #print(chardit1)
+
+    html=html.decode(chardit1['encoding'])
+
+    #print(html)
+
+    pattern = re.compile("<p.*?</P>" ,re.S)
+    pattern2 = re.compile("<h2.*?>.*?</h2>" ,re.S)
+    pattern3 = re.compile("<span class=\"time.*?</span>" ,re.S)
+    #pattern = re.compile("<div.*?class=\"articalContent.*?\">" ,re.S)
+    title=re.search(pattern2, html).group()
+    time=re.search(pattern3, html).group()
+    index1=title.find(">")
+    index2=title.find("</h2>")
+    title=title[index1+1:index2]
+    time=time[-28:-7]
+    ps=re.findall(pattern, html)
+    #print(ps)
+    '''
+    for i in ps:
+        f=i.replace("\n", "").replace("<p>", "").replace("</P>", "")
+        print(f)
+
+    '''
+    artical=[]
+    for i in ps:
+        #f=i
+        f=i.replace("&nbsp;<wbr>", "").replace("\n", "").replace("<p>", "").replace("</P>", "")
+        if "<" in f:
+            pass
+        elif f=="" or f=="\n":
+            pass
+        else:
+            artical.append(f)
+    
+    dic={}
+    dic["title"]=title
+    dic["time"]=time
+    dic["artical"]=artical
+    return dic
+
+for i in l:
+    dic=get_artical(i)
+    with open("vaeblog.txt", 'a+') as f:
+        f.write(dic["title"]+"\n")
+        f.write(dic["time"]+"\n")
+        f.write(i+"\n")
+        for j in dic["artical"]:
+            f.write(j+"\n")
+        #f.writelines(dic["artical"])
+        f.write("\n\n")
+        print(dic["title"])
+    
